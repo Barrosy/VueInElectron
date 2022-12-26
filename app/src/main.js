bcfece378896116af9e3b90c93a4367f5a686ca6
Barry Stavenuiter <barry.stavenuiter@hva.nl>
2022-12-26 23:49:08 +0100

    Vue Router 3 init

--- app/src/main.js
+++ app/src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
 Vue.config.productionTip = false
 
-console.log(Vue.version)
-
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
