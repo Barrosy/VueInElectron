b10b30aee9c65de2ecaaab83a3edce7268daac9a
Barry Stavenuiter <barry.stavenuiter@hva.nl>
2022-12-26 18:57:38 +0100

    Re-initialize with older version of Vue, Vue 2.2

--- app/src/main.js
+++ app/src/main.js
@@ -1,7 +1,11 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
+
 import App from './App.vue'
 import router from './router'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap/dist/js/bootstrap.min.js"
 
-createApp(App).use(router).mount('#app')
+import './assets/main.css'
+
+new Vue({
+  router,
+  render: (h) => h(App)
+}).$mount('#app')
--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,25 +1,27 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
-const routes = [
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  base: import.meta.env.BASE_URL,
+  routes: [
     {
-        path: '/',
-        name: 'home',
-        component: HomeView
+      path: '/',
+      name: 'home',
+      component: HomeView
     },
     {
-        path: '/about',
-        name: 'about',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
     }
-]
-
-const router = createRouter({
-    history: createWebHashHistory(process.env.BASE_URL),
-    routes
+  ]
 })
 
 export default router
